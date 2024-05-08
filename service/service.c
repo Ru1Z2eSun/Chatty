8bf52826b402495aa650ce9f043afec565d21a96
Ru1Z2eSun <139456232+Ru1Z2eSun@users.noreply.github.com>
2024-05-08 16:19:43 +0800

    Update service.c

--- service/service.c
+++ service/service.c
@@ -1,56 +1,79 @@
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <arpa/inet.h>
 #include <unistd.h>
-
 #include "protocol.h"
+
 
 #define PORT 8888
 #define IP "127.0.0.1"
 #define BACKLOG 1024
 
-int main() {
-    int ServerSocket, ClientSocket;
+int LoginCheck(struct UserPwd user)
+{
+
+}
+
+int main()
+{
+    int ServerSocket,ClientSocket;
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_addr_len = sizeof(client_addr);
-    char *buffer;
-    int rcv = -1;
+    struct packet* buffer,*reply;
+    int rcv=-1;
+    u_int16_t *meth,*len;
+    u_int8_t data[4064];
 
-    ServerSocket = socket(AF_INET, SOCK_STREAM, 0);
+    ServerSocket=socket(AF_INET,SOCK_STREAM,0);
     if (ServerSocket == -1) {
         perror("Failed to create socket");
         return -1;
     }
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_family=AF_INET;
+    server_addr.sin_port=htons(PORT);
+    server_addr.sin_addr.s_addr=htonl(INADDR_ANY);
 
-    if (bind(ServerSocket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+    if(bind(ServerSocket,(struct sockaddr *)&server_addr,sizeof(server_addr))==-1)
+    {
         perror("Failed to bind on port");
         return -1;
     }
 
-    if (listen(ServerSocket, BACKLOG) == -1) {
+    if(listen(ServerSocket,BACKLOG)==-1)
+    {
         perror("Failed to listen");
         return -1;
     }
-    ClientSocket = accept(ServerSocket, (struct sockaddr *)&client_addr, &client_addr_len);
-    if (ClientSocket == -1) {
+    ClientSocket=accept(ServerSocket,(struct sockaddr *)&client_addr,&client_addr_len);
+    if(ClientSocket==-1)
+    {
         perror("fail to connection");
         return -1;
     }
-    // connected
-    while (1) {
-        memset(buffer, 0, sizeof(buffer));
-        if (read(ClientSocket, buffer, sizeof(buffer)) <= 0) {
+    //connected
+    while(1)
+    {
+        memset(buffer,0,sizeof(buffer));
+        memset(reply,0,sizeof(reply));
+        rcv=recv(ClientSocket,buffer,sizeof(buffer),0);
+        *meth=&buffer;
+        *len=&buffer+8;
+        if(rcv==-1)
+        {
             close(ClientSocket);
             continue;
         }
-        if (write(ClientSocket, buffer, sizeof(buffer)) < 0) {
+
+        if(buffer->method==login)
+        {
+            
+        }
+        if(write(ClientSocket,buffer,sizeof(buffer))<0)
+        {
             close(ClientSocket);
             continue;
         }
